569dca5b0e8069f52e6a097c8019021ff0309c73
王锦涛 <jintao9@staff.weibo.com>
2020-12-14 23:39:10 +0800

    模拟推流接口

--- src/main/java/wjt/MyAnnotationConfigDispatcherServletInitializer.java
+++ src/main/java/wjt/MyAnnotationConfigDispatcherServletInitializer.java
@@ -4,6 +4,14 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 import wjt.config.BizConfig;
 import wjt.config.WebConfig;
+import wjt.filter.CorsFilter;
+
+import javax.servlet.DispatcherType;
+import javax.servlet.Filter;
+import javax.servlet.FilterRegistration;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletRegistration;
+import java.util.EnumSet;
 
 @Slf4j
 public class MyAnnotationConfigDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
@@ -21,4 +29,22 @@ public class MyAnnotationConfigDispatcherServletInitializer extends AbstractAnno
     protected String[] getServletMappings() {
         return new String[]{"*.json"};
     }
+
+    @Override
+    protected Filter[] getServletFilters() {
+        return new Filter[]{new CorsFilter()};
+    }
+
+    @Override
+    protected FilterRegistration.Dynamic registerServletFilter(ServletContext servletContext, Filter filter) {
+        FilterRegistration.Dynamic filterRegistration = servletContext.addFilter("corsFilter", filter);
+
+        EnumSet<DispatcherType> dispatcherTypes = EnumSet.allOf(DispatcherType.class);
+
+        filterRegistration.addMappingForUrlPatterns(dispatcherTypes, false, "/*");
+
+        log.info("filter={};registration!", filter);
+        return filterRegistration;
+    }
+
 }
---
+++ src/main/java/wjt/filter/CorsFilter.java
@@ -0,0 +1,39 @@
+package wjt.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.annotation.Order;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Slf4j
+@Order(value = 1)
+//@WebFilter(urlPatterns = {"/*"})
+public class CorsFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        log.info("corsFilter init finish!");
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+        httpServletResponse.setHeader("Access-Control-Allow-Origin", "*"); //  这里最好明确的写允许的域名
+        httpServletResponse.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
+        httpServletResponse.setHeader("Access-Control-Max-Age", "3600");
+        httpServletResponse.setHeader("Access-Control-Allow-Headers", "Content-Type,Access-Token,Authorization,ybg");
+        log.info("cors allow!");
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
